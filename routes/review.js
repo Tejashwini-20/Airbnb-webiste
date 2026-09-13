69d57a32a501e500e16fbe4230178b759ab02835
Tejashwini <tejashwinisingwal>
2026-09-13 13:11:45 +0530

    fix vercel middlewares

--- routes/review.js
+++ routes/review.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router({mergeParams:true});
 const wrapAsync =require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
-const {validateReview,isLoggedIn,isReviewAuthor} = require("../middleware.js");
+const {validateReview,isLoggedIn,isReviewAuthor} = require("../middlewares.js");
 
 const Review= require("../models/review.js");
 const Listing= require("../models/listing.js");
